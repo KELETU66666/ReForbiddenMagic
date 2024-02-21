5c52f21ff3d0c7fe37345222fac732a93d6b9f6c
a <EYCHFSJD@gmail.com>
2024-02-21 15:14:48 +0800

    fix

--- src/main/java/keletu/forbiddenmagicre/compat/psi/Psionics.java
+++ src/main/java/keletu/forbiddenmagicre/compat/psi/Psionics.java
@@ -1,30 +1,36 @@
 package keletu.forbiddenmagicre.compat.psi;
 
 import fox.spiteful.lostmagic.Lumberjack;
+import keletu.forbiddenmagicre.util.Reference;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Loader;
 import org.apache.logging.log4j.Level;
 import vazkii.psi.api.PsiAPI;
+import vazkii.psi.api.spell.SpellPiece;
+
+import static vazkii.psi.api.PsiAPI.registerSpellPiece;
+import static vazkii.psi.api.PsiAPI.simpleSpellTextures;
 
 public class Psionics {
+
+    private static void registerSpellPieceAndTexture(String key, Class<? extends SpellPiece> clazz) {
+        registerSpellPiece(key, clazz);
+        String textureName = key.replaceAll("([a-z0-9])([A-Z])", "$1_$2").toLowerCase();
+        if(Loader.isModLoaded("magipsi")) {
+            simpleSpellTextures.put(key, new ResourceLocation(Reference.MOD_ID, String.format("textures/spell/magipsi/%s.png", textureName)));
+        }else
+            simpleSpellTextures.put(key, new ResourceLocation(Reference.MOD_ID, String.format("textures/spell/%s.png", textureName)));
+    }
 
     public static void oneechan(){
 
         try {
             try {
-                if(Loader.isModLoaded("magipsi")){
-                    PsiAPI.registerSpellPieceAndTexture("magipsi/trick_warpward", PieceTrickWarpWard.class);
-                    PsiAPI.registerSpellPieceAndTexture("magipsi/trick_deathgaze", PieceTrickDeathGaze.class);
-                    PsiAPI.registerSpellPieceAndTexture("magipsi/trick_fluxflu", PieceTrickFluxFlu.class);
-                    PsiAPI.registerSpellPieceAndTexture("magipsi/trick_fluxtaint", PieceTrickFluxTaint.class);
-                    PsiAPI.registerSpellPieceAndTexture("magipsi/trick_siphonaura", PieceTrickSiphonAura.class);
-                }
-                else {
-                    PsiAPI.registerSpellPieceAndTexture("trick_warpward", PieceTrickWarpWard.class);
-                    PsiAPI.registerSpellPieceAndTexture("trick_deathgaze", PieceTrickDeathGaze.class);
-                    PsiAPI.registerSpellPieceAndTexture("trick_fluxflu", PieceTrickFluxFlu.class);
-                    PsiAPI.registerSpellPieceAndTexture("trick_fluxtaint", PieceTrickFluxTaint.class);
-                    PsiAPI.registerSpellPieceAndTexture("trick_siphonaura", PieceTrickSiphonAura.class);
-                }
+                registerSpellPieceAndTexture("trick_warpward", PieceTrickWarpWard.class);
+                registerSpellPieceAndTexture("trick_deathgaze", PieceTrickDeathGaze.class);
+                registerSpellPieceAndTexture("trick_fluxflu", PieceTrickFluxFlu.class);
+                registerSpellPieceAndTexture("trick_fluxtaint", PieceTrickFluxTaint.class);
+                registerSpellPieceAndTexture("trick_siphonaura", PieceTrickSiphonAura.class);
                 PsiAPI.setGroupRequirements("thaumic", 25, "eidosReversal");
                 PsiAPI.addPieceToGroup(PieceTrickWarpWard.class, "thaumic", false);
                 PsiAPI.addPieceToGroup(PieceTrickDeathGaze.class, "thaumic", false);
@@ -37,18 +43,10 @@ public class Psionics {
             }
             try {
                 if(Loader.isModLoaded("botania")) {
-                    if(Loader.isModLoaded("magipsi")){
-                        PsiAPI.registerSpellPieceAndTexture("magipsi/trick_genmana", PieceTrickGenMana.class);
-                        PsiAPI.registerSpellPieceAndTexture("magipsi/trick_soulcross", PieceTrickSoulCross.class);
-                        PsiAPI.registerSpellPieceAndTexture("magipsi/trick_allure", PieceTrickAllure.class);
-                        PsiAPI.registerSpellPieceAndTexture("magipsi/trick_emptiness", PieceTrickEmptiness.class);
-                    }
-                    else {
-                        PsiAPI.registerSpellPieceAndTexture("trick_genmana", PieceTrickGenMana.class);
-                        PsiAPI.registerSpellPieceAndTexture("trick_soulcross", PieceTrickSoulCross.class);
-                        PsiAPI.registerSpellPieceAndTexture("trick_allure", PieceTrickAllure.class);
-                        PsiAPI.registerSpellPieceAndTexture("trick_emptiness", PieceTrickEmptiness.class);
-                    }
+                    registerSpellPieceAndTexture("trick_genmana", PieceTrickGenMana.class);
+                    registerSpellPieceAndTexture("trick_soulcross", PieceTrickSoulCross.class);
+                    registerSpellPieceAndTexture("trick_allure", PieceTrickAllure.class);
+                    registerSpellPieceAndTexture("trick_emptiness", PieceTrickEmptiness.class);
                     PsiAPI.setGroupRequirements("botany", 25, "eidosReversal");
                     PsiAPI.addPieceToGroup(PieceTrickGenMana.class, "botany", true);
                     PsiAPI.addPieceToGroup(PieceTrickSoulCross.class, "botany", false);
